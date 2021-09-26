3be720550866b73653f0768e3bb3c8b614e1be10
trungle11 <le.thanh.trung@sun-asterisk.com>
2021-09-26 23:01:12 +0700

    Handle register new account

--- e-commerce-website/src/app/reducer.js
+++ e-commerce-website/src/app/reducer.js
@@ -1,9 +1,11 @@
 import productsReducer from 'src/pages/Products/products.slice'
 import allProductsReducder from 'src/pages/Products/allProducts.slice'
+import authReducer from 'src/pages/Auth/auth.slice'
 
 const rootReducer = {
   products: productsReducer,
-  allProducts: allProductsReducder
+  allProducts: allProductsReducder,
+  auth: authReducer
 }
 
 export default rootReducer
