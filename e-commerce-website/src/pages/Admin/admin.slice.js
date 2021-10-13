747421ef4d7c9aa29388782a5ab3170ca8a4d5e5
trungle11 <le.thanh.trung@sun-asterisk.com>
2021-10-13 14:32:42 +0700

    Handle manage user by admin

--- e-commerce-website/src/pages/Admin/admin.slice.js
+++ e-commerce-website/src/pages/Admin/admin.slice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { payLoadCreater } from 'src/utils/helper'
+import { findIndexById, payLoadCreater } from 'src/utils/helper'
 import authApi from 'src/api/auth.api'
 
 export const fetchAllUsers = createAsyncThunk(
@@ -7,14 +7,42 @@ export const fetchAllUsers = createAsyncThunk(
   payLoadCreater(authApi.fetchAllUsers)
 )
 
+export const updateUser = createAsyncThunk(
+  'admin/updateUser',
+  payLoadCreater(authApi.updateUser)
+)
+
 const admin = createSlice({
   name: 'admin',
   initialState: {
-    allUsers: []
+    allUsers: [],
+    loading: false,
+    error: ''
   },
   extraReducers: {
+    [fetchAllUsers.pending]: (state, action) => {
+      state.loading = true
+    },
     [fetchAllUsers.fulfilled]: (state, action) => {
+      state.loading = false
       state.allUsers = action.payload.data
+    },
+    [fetchAllUsers.rejected]: (state, action) => {
+      state.loading = false
+      state.error = action.payload
+    },
+    [updateUser.pending]: (state, action) => {
+      state.loading = true
+    },
+    [updateUser.fulfilled]: (state, action) => {
+      state.loading = false
+      const user = action.payload.data
+      const itemIndex = findIndexById(user, state.allUsers)
+      state.allUsers.splice(itemIndex, 1, user)
+    },
+    [updateUser.rejected]: (state, action) => {
+      state.loading = false
+      state.error = action.payload
     }
   }
 })
